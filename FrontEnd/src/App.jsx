b72cafa2ffb7b0dc60a3479401bc5d3a7bdb8b10
SakshamSaini1001 <sakshamsaini654@gmail.com>
2024-09-17 15:53:51 +0530

    cryto

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,28 +1,26 @@
+import "./App.css";
+import Navbar from "./page/Navbar/Navbar";
+import Home from "./page/Home/home";
+import { Routes, Route } from "react-router-dom";
 
-import './App.css'
-import Navbar from './page/Navbar/Navbar'
-import Home from './page/Home/home' 
-import { Routes , Route} from 'react-router-dom'
-
-import Portfolio from './page/Portfolio/Portfolio'
-import Activity from './page/Activity/Activity'
-import Withdrawal from './page/Withdrawal/Withdrawal'
-import PaymentDetails from './page/Payment Details/PaymentDetails'
-import StockDetails from './page/Stock Details/StockDetails' 
-import WatchList from './page/WatchList/WatchList' 
-import Profile from './page/Profile/Profile' 
-import SearchCoin from './page/Search/Search' 
-import NotFound from './page/NotFound/NotFound'
-import Wallet from './page/Wallet/Wallet'
-import Auth from './page/Auth/Auth.jsx'  
-import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from 'react'
-import { getUser } from './page/State/Auth/Action.js'
+import Portfolio from "./page/Portfolio/portfolio";
+import Activity from "./page/Activity/Activity";
+import Withdrawal from "./page/Withdrawal/Withdrawal";
+import PaymentDetails from "./page/Payment Details/PaymentDetails";
+import StockDetails from "./page/Stock Details/StockDetails";
+import WatchList from "./page/WatchList/WatchList";
+import Profile from "./page/Profile/Profile";
+import SearchCoin from "./page/Search/Search";
+import NotFound from "./page/NotFound/NotFound";
+import Wallet from "./page/Wallet/Wallet";
+import Auth from "./page/Auth/Auth.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getUser } from "./page/State/Auth/Action.js";
 
 function App() {
-
-  const {auth} = useSelector(store=>store);
-  const dispatch=useDispatch();
+  const { auth } = useSelector((store) => store);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (auth.jwt) {
@@ -37,28 +35,30 @@ function App() {
 
   return (
     <>
-    {auth.jwt ?
-    <div>
-      <Navbar/>
-      <Routes>
-      <Route path="/" element={<Home/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/portfolio" element={<Portfolio/>}/>
-        <Route path="/activity" element={<Activity/>}/>
-        <Route path="/wallet" element={<Wallet/>}/>
-        <Route path="/withdrawal" element={<Withdrawal/>}/>
-        <Route path="/payment-details" element={<PaymentDetails/>}/>
-        <Route path="/home/market/:id" element={<StockDetails/>}/>
-        <Route path="/market/:id" element={<StockDetails/>}/>
-        <Route path="/watchlist" element={<WatchList/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/search" element={<SearchCoin/>}/>
-        <Route path="/*" element={<NotFound/>}/>
-
-      </Routes>
-      </div>:(<Auth/>)}
+      {auth.jwt ? (
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/activity" element={<Activity />} />
+            <Route path="/wallet" element={<Wallet />} />
+            <Route path="/withdrawal" element={<Withdrawal />} />
+            <Route path="/payment-details" element={<PaymentDetails />} />
+            <Route path="/home/market/:id" element={<StockDetails />} />
+            <Route path="/market/:id" element={<StockDetails />} />
+            <Route path="/watchlist" element={<WatchList />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/search" element={<SearchCoin />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </div>
+      ) : (
+        <Auth />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
